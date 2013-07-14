6b8ad101722dcb0bc3792eca4ee5428615d4e564
Solly Ross <sross@redhat.com>
2013-07-14 08:13:41 -0400

    added py33 to tox, fixed setup.py's use of commands

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@ import setuptools
 from setuptools import setup
 from setuptools import Extension
 import sys
-import commands
+
+get_output = None
+
+try:
+    import commands
+    get_output = commands.getoutput
+except ImportError:
+    import subprocess
+    get_output = lambda *a: subprocess.check_output(*a, shell=True)
 
 ext_module_b = Extension(
     'gssapi.base.impl',
-    extra_link_args = commands.getoutput('krb5-config --libs gssapi').split(),
-    extra_compile_args = commands.getoutput('krb5-config --cflags gssapi').split(),
+    extra_link_args = get_output('krb5-config --libs gssapi').split(),
+    extra_compile_args = get_output('krb5-config --cflags gssapi').split(),
 #   include_dirs=['/home/sross/pydebug/include/python2.7'],
     sources = [
         'sys_src/gssapi.base.impl.c'
@@ -17,8 +25,8 @@ ext_module_b = Extension(
 
 ext_module_ct = Extension(
     'gssapi.base.ctypes',
-    extra_link_args = commands.getoutput('krb5-config --libs gssapi').split(),
-    extra_compile_args = commands.getoutput('krb5-config --cflags gssapi').split(),
+    extra_link_args = get_output('krb5-config --libs gssapi').split(),
+    extra_compile_args = get_output('krb5-config --cflags gssapi').split(),
 #   include_dirs=['/home/sross/pydebug/include/python2.7'],
     sources = [
         'sys_src/gssapi.base.ctypes.c'
@@ -27,8 +35,8 @@ ext_module_ct = Extension(
 
 ext_module_su = Extension(
     'gssapi.base.status_utils',
-    extra_link_args = commands.getoutput('krb5-config --libs gssapi').split(),
-    extra_compile_args = commands.getoutput('krb5-config --cflags gssapi').split(),
+    extra_link_args = get_output('krb5-config --libs gssapi').split(),
+    extra_compile_args = get_output('krb5-config --cflags gssapi').split(),
 #   include_dirs=['/home/sross/pydebug/include/python2.7'],
     sources = [
         'sys_src/gssapi.base.status_utils.c'
